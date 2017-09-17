3432e80df3d85489893d6b3046fa55a6254531e7
mrbergeron83 <mr.bergeron83@gmail.com>
2017-09-17 13:53:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var configJS = {
     port: process.env.PORT || 3000,
-    dbUrl: "mongodb://bbadmin:bbadmin@ds145302.mlab.com:45302/billboard"
+    dbUrl: "mongodb://**:**@ds145302.mlab.com:45302/billboard"
 }
 
 module.exports = configJS;
